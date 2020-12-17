5e20b1898762539abcbea8ec1849ea2f3f5ce2d1
Muramatsu2602 <pedromuramatsuc@gmail.com>
2020-12-17 10:07:22 -0300

    adding stuff to routers

--- node/src/index.js
+++ node/src/index.js
@@ -1,60 +1,18 @@
-// creating a server: basics
-// const express = require('express');
-import express from 'express'; // melhor esse
+import express from 'express';
 import bodyParser from 'body-parser';
 
-const server = express();
+import studentsRouter from './routers/studentsRouter';
+
+const server = express(); // 0080
+
 server.use(bodyParser.urlencoded({
     extended: true
 }));
-server.use(bodyParser.json());
 
-// server.listen(3000, function(){ // porta 80 eh padrao para localhost
-//     console.log("rodando");
-// });
+server.use(bodyParser.json())
 
-// server.get("/",(request, response) => {
-//     response.send("<h1>OI<\h1>");
+server.use('/alunos', studentsRouter);
 
-// });
-
-let students;
-
-server.get("/alunos", (req, res) => {});
-
-// server.post("/alunos", {req, res} => {
-//      
-// });
-
-server.get("/alunos/:id", (req, res) => {
-    const id = req.params.id;
-
-    // const students = [{id:1},{id:2}]
-    let searchedstudent;
-
-    students.forEach(student => {
-        if (student.id === parseInt(id)) {
-            searchedstudent = student;
-        }
-    });
+server.listen(80, function () {
+    console.log("Rodando");
 });
-
-server.put("/alunos/id:")
-
-
-let alunos = [{
-        id: 1,
-        name: 'Aluno 1',
-        age: 14
-    }, // pelo id eh garantido que outros atributos nao se repitam
-    {
-        id: 2,
-        name: 'Aluno 2',
-        age: 24
-    },
-    {
-        id: 3,
-        name: 'Aluno 3',
-        age: 53
-    }
-]
---
+++ node/src/routers/studentsRouter.js
@@ -0,0 +1,84 @@
+import express from 'express';
+
+const router = express.Router();
+
+let students = [];
+
+router.get("/", (req, res) => {
+    return res.status(200).send(students);
+});
+
+router.post("/", (req, res) => {
+    const {
+        name,
+        age
+    } = req.body;
+
+    const student = {
+        id: students.length + 1,
+        name,
+        age
+    };
+
+    students.push(student);
+
+    return res.status(202).send(student);
+});
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+
+    let searchedStudent;
+
+    students.forEach(student => {
+        if (student.id === parseInt(id)) {
+            searchedStudent = student;
+        }
+    })
+
+    return res.status(200).send(searchedStudent);
+});
+
+router.put("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const {
+        name,
+        age
+    } = req.body;
+
+    let newStudents = [];
+    const newStudent = {
+        id,
+        name,
+        age
+    };
+
+    students.forEach(student => {
+        if (student.id === id) {
+            newStudents.push(newStudent);
+        } else newStudents.push(student);
+    })
+
+    students = newStudents;
+
+    return res.status(200).send(newStudent);
+});
+
+router.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    let newStudents = [];
+
+    students.forEach(student => {
+        if (student.id !== id) {
+            newStudents.push(student);
+        }
+    })
+
+    students = newStudents;
+
+    return res.status(200).send(newStudents);
+
+});
+
+export default router;
