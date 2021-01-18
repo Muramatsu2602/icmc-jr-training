c2bf8e2487185a7229c0d7418c814829c2c2f3c5
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-17 21:48:55 -0300

    showing the table on screen

--- react-tutorial/pokelist/src/pages/home/index.js
+++ react-tutorial/pokelist/src/pages/home/index.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 /**
  * useState -> consegue saber quando certa variavel foi alterada
  * useEffect -> vc passa uma funcao nele
@@ -7,30 +7,33 @@ import axios from 'axios';
  */
 
 function Header() {
-
-  return (
-
-
-    <div>
-
-    </div>
-  );
+  return <div></div>;
 }
 
-function FirstTable() {
+function FirstTable(props) {
   const [pokeList, setPokelist] = useState([]);
+  const [pokeListOffset, setPokeListOffset] = useState(0);
 
   const fetchData = async () => {
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/?offset=${pokeListOffset}&limit=20`
+      );
 
-    for(const pokemon of response.data) {
-      
+      let vetor = [];
+      for (const pokemon of response.data.results) {
+        const _response = await axios.get(pokemon.url);
+        vetor.push(_response.data);
+      }
+      setPokelist(vetor.data);
+    } catch (e) {
+      console.log(e.message);
     }
-  }
+  };
 
   useEffect(() => {
-
-  }, []);
+    fetchData();
+  }, [pokeListOffset]);
 
   return (
     <div>
@@ -42,19 +45,75 @@ function FirstTable() {
           <th>Image</th>
           <th>Fav</th>
         </tr>
-        {
+        {pokeList.length > 0 &&
+          pokeList.map((pokemon) => {
+            return (
+              <tr>
+                <td>{pokemon.name}</td>
+                <td>
+                  {pokemon.types.map((type) => {
+                    return type.type.name + " ";
+                  })}
+                </td>
 
-        }
+                <td>{pokemon.weight}</td>
+
+                <td>
+                  <img src={pokemon.sprites.font_default} />
+                </td>
+
+                <td>
+                  {props.searchPokemon(pokemon) ? (
+                    <a
+                      href="#"
+                      onClick={() =>
+                        props.pokeController.deletePokemon(pokemon)
+                      }
+                    >
+                      Desfavoritar
+                    </a>
+                  ) : (
+                    <a
+                      href="#"
+                      onClick={() => props.pokeController.savePokemon(pokemon)}
+                    >
+                      Favoritar
+                    </a>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
       </table>
+      <div>
+        <a href="#" onClick={() => setPokeListOffset(pokeListOffset + 20)}>
+          Next
+        </a>
+      </div>
     </div>
   );
 }
 
-
 function SecondTable() {
+  return <div></div>;
+}
+
+function Body(props) {
+  const [favPokeList, setFavPokeList] = useState([]);
+  const [pokeController, setPokeController] = useState({
+    savePokemon: (pokemon) => {},
+    deletePokemon: (pokemon) => {},
+    searchPokemon: (pokemon) => {},
+  });
+
+  useEffect(() => {
+    setFavPokeList(JSON.parse(localStorage.getItem("favoritesPokemons")));
+  }, []);
+
   return (
     <div>
-
+      <FirstTable favPokelist={favPokeList} pokeController={pokeController} />
+      <SecondTable favPokelist={favPokeList} pokeController={pokeController} />
     </div>
   );
 }
@@ -64,10 +123,10 @@ function Home() {
     <div>
       <Header />
       <h2></h2>
-      <div>
+      <Body>
         <FirstTable />
         <SecondTable />
-      </div>
+      </Body>
     </div>
   );
 }
