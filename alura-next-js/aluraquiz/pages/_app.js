97f485b30bc8c33f51c2ca79f32f579897ac5efc
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-26 00:04:35 -0300

    components in next.js

--- alura-next-js/aluraquiz/pages/_app.js
+++ alura-next-js/aluraquiz/pages/_app.js
@@ -1,18 +1,19 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color: grey;
   }
-`
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +23,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- alura-next-js/aluraquiz/pages/index.js
+++ alura-next-js/aluraquiz/pages/index.js
@@ -1,10 +1,18 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
+
+// function Title(props){
+//   return (
+//     <h1>
+//       {props.children}
+//     </h1>
+//   );
+// }
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title>My page</Title>;
 }
