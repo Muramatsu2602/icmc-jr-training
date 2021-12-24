6078da01c6ba92706c61a309e337eb6a7e6422d9
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-12-23 23:42:11 -0300

    Using types in when returning

--- alura-courses/typescript-1/app/controllers/negociacao-controller.ts
+++ alura-courses/typescript-1/app/controllers/negociacao-controller.ts
@@ -1,8 +1,8 @@
 import { Negociacao } from './../models/negociacao.js'
 export class NegociacaoController {
-	private inputData
-	private inputQuantidade
-	private inputValor
+	private inputData: HTMLInputElement
+	private inputQuantidade: HTMLInputElement
+	private inputValor: HTMLInputElement
 
 	constructor() {
 		this.inputData = document.querySelector('#data')
@@ -10,13 +10,17 @@ export class NegociacaoController {
 		this.inputValor = document.querySelector('#valor')
 	}
 
-	adiciona() {
-		const negociacao = new Negociacao(
-			this.inputData.value,
-			this.inputQuantidade.value,
-			this.inputValor.value
-		)
-
+	adiciona(): void {
+		const negociacao: Negociacao = this.criaNegociacao()
 		console.log(negociacao)
+	}
+
+	criaNegociacao(): Negociacao {
+		const exp = /-/g
+		const date = new Date(this.inputData.value.replace(exp, ','))
+		const quantidade = parseInt(this.inputQuantidade.value)
+		const valor = parseInt(this.inputQuantidade.value)
+
+		return new Negociacao(date, quantidade, valor)
 	}
 }
