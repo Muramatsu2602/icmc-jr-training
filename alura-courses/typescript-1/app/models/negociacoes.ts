cfa3a70091cf528f53b0725144d09fe219e6a366
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-12-23 23:58:05 -0300

    More on generics and playing with spread operator

--- alura-courses/typescript-1/app/models/negociacoes.ts
+++ alura-courses/typescript-1/app/models/negociacoes.ts
@@ -2,9 +2,13 @@ import { Negociacao } from './negociacao.js'
 
 export class Negociacoes {
 	private negociacoes: Array<Negociacao> = []
+
+	adiciona(negociacao: Negociacao) {
+		this.negociacoes.push(negociacao)
+	}
+
+	lista(): Array<Negociacao> {
+		return [...this.negociacoes] // take every item in this list and put in a NEW list
+                                     // thus, the original negociacoes is NOT affected
+	}
 }
-
-// const list: Array<string> = []
-
-// list.push("Joao Carlos")
-// list.push("Carlos Antunes")
