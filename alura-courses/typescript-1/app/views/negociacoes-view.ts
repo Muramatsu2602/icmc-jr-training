6d6d238ed03c1820d5d8e4c42a2c31d9d15cb9ea
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-12-25 00:24:18 -0300

    Creating a view

---
+++ alura-courses/typescript-1/app/views/negociacoes-view.ts
@@ -0,0 +1,19 @@
+export class NegociacoesView {
+	// returns HTML string + fused data
+	template(): string {
+		return `
+        <table class="table table-hover table-bordered>
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+        `
+	}
+}
