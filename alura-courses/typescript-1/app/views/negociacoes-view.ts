23a2b2b9607fc2ae4452a9a24480dc4579836b0b
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-12-26 22:48:20 -0300

    Adding template() and update() in view class

--- alura-courses/typescript-1/app/views/negociacoes-view.ts
+++ alura-courses/typescript-1/app/views/negociacoes-view.ts
@@ -1,4 +1,10 @@
 export class NegociacoesView {
+	private elemento: HTMLElement
+
+	constructor(seletor: string) {
+		this.elemento = document.querySelector(seletor)
+	}
+
 	// returns HTML string + fused data
 	template(): string {
 		return `
@@ -15,5 +21,10 @@ export class NegociacoesView {
             </tbody>
         </table>
         `
+	}
+
+	// render template in the captured element in constructor
+	update(): void {
+		this.elemento.innerHTML = this.template()
 	}
 }
