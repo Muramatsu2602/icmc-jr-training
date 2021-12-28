08557386330b95f34e600a27165217d001fb889a
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-12-27 21:58:26 -0300

    Adding an observation to negociacoes-view.ts

--- alura-courses/typescript-1/app/views/view.ts
+++ alura-courses/typescript-1/app/views/view.ts
@@ -1,5 +1,6 @@
 export class View {
-	private element: HTMLElement
+	// protected --> inherited classes may access it
+	protected element: HTMLElement
 
 	constructor(seletor: string) {
 		this.element = document.querySelector(seletor)
