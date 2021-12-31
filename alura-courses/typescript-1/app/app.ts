ba21eaf1ff292f1d8550d8413cf5b02402dace7a
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-12-30 23:09:46 -0300

    casting adaptions using TS

--- alura-courses/typescript-1/app/app.ts
+++ alura-courses/typescript-1/app/app.ts
@@ -1,13 +1,11 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js'
 
 const controller = new NegociacaoController()
-const form = document.querySelector('.form')
+const form = document.querySelector('.form') as Element
 form.addEventListener('submit', (event) => {
 	event.preventDefault()
 	controller.adiciona()
-})
-
-
+}) 
 
 // one variable --> multiple allowed data types
 // const x: string | number | boolean = true
