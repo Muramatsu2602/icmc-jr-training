6b680275016a4c23cdf0c3aa9d89bb5d907b76bd
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-12-31 23:16:53 -0300

    testing time performance of view.js's update()

--- alura-courses/typescript-1-to-3/app/src/views/view.ts
+++ alura-courses/typescript-1-to-3/app/src/views/view.ts
@@ -1,28 +1,32 @@
 export abstract class View<T> {
+	protected elemento: HTMLElement
+	private escapar = false
 
-    protected elemento: HTMLElement;
-    private escapar = false;
+	constructor(seletor: string, escapar?: boolean) {
+		const elemento = document.querySelector(seletor)
+		if (elemento) {
+			this.elemento = elemento as HTMLElement
+		} else {
+			throw Error(`Seletor ${seletor} não existe no DOM. Verifique`)
+		}
+		if (escapar) {
+			this.escapar = escapar
+		}
+	}
 
-    constructor(seletor: string, escapar?: boolean) {
-        const elemento = document.querySelector(seletor);
-        if (elemento) {
-            this.elemento = elemento as HTMLElement;
-        } else {
-            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
-        }
-        if (escapar) {
-            this.escapar = escapar;
-        }
-    }
+	public update(model: T): void {
+		const t1 = performance.now()
 
-    public update(model: T): void {
-        let template = this.template(model);
-        if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
-        this.elemento.innerHTML = template;
-    }
+		let template = this.template(model)
+		if (this.escapar) {
+			template = template.replace(/<script>[\s\S]*?<\/script>/, '')
+		}
+		this.elemento.innerHTML = template
 
-    protected abstract template(model: T): string;
+		const t2 = performance.now()
+
+		console.log(`execution time for update() ${(t2 - t1) / 1000}`)
+	}
+
+	protected abstract template(model: T): string
 }
