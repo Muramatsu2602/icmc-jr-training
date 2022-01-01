bbc9bc3356e8842cec6c48f35e1ac0cadbaee961
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-12-31 23:32:13 -0300

    creating LogExecTime() decorator

--- alura-courses/typescript-1-to-3/app/src/decorators/log-exec-time.ts
+++ alura-courses/typescript-1-to-3/app/src/decorators/log-exec-time.ts
@@ -1,6 +1,20 @@
 // my first ever decorator in TS
 export function LogExecTime() {
-    
+	return function (
+		target: any,
+		propertyKey: string,
+		descriptor: PropertyDescriptor
+	) {
+		const originalMethod = descriptor.value
+		descriptor.value = function (...args: Array<any>) {
+			const t1 = performance.now()
+			const originalReturn = originalMethod.apply(this, args)
+			const t2 = performance.now()
+            
+			console.log(`${propertyKey}, execution time: ${(t2 - t1) / 1000}`)
+			originalReturn
+		}
 
-
+		return descriptor
+	}
 }
