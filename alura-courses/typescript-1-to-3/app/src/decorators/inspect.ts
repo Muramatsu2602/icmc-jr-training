02149dee808d0f59fed9fd6e42b9792eded1493b
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2022-01-01 23:54:13 -0300

    creating inspect decorator

---
+++ alura-courses/typescript-1-to-3/app/src/decorators/inspect.ts
@@ -0,0 +1,20 @@
+export function inspect() {
+	return function (
+		target: any,
+		propertyKey: string,
+		descriptor: PropertyDescriptor
+	) {
+		const originalMethod = descriptor.value
+
+		descriptor.value = function (...args: any[]) {
+			console.log(`--- Method: ${propertyKey}`)
+			console.log(`--- Parameters: ${JSON.stringify(args)}`)
+			const originalReturn = originalMethod.apply(this, args)
+			console.log(`--- Returns: ${originalReturn}`)
+
+			return originalReturn
+		}
+
+		return descriptor
+	}
+}
--- alura-courses/typescript-1-to-3/app/src/decorators/log-exec-time.ts
+++ alura-courses/typescript-1-to-3/app/src/decorators/log-exec-time.ts
@@ -7,11 +7,21 @@ export function LogExecTime(inSeconds: boolean = false) {
 	) {
 		const originalMethod = descriptor.value
 		descriptor.value = function (...args: Array<any>) {
+			let div = 1
+			let un = 'milliseconds'
+			if (inSeconds) {
+				div = 1000
+				un = 'seconds'
+			}
+
 			const t1 = performance.now()
 			const originalReturn = originalMethod.apply(this, args)
 			const t2 = performance.now()
 
-			console.log(`${propertyKey}, execution time: ${(t2 - t1) / 1000}`)
+			console.log(
+				`${propertyKey}, execution time: ${(t2 - t1) / div} ${un}`
+			)
+            
 			originalReturn
 		}
 
