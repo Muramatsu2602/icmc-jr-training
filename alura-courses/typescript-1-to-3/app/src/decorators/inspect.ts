2bb743e637244c4976f21505f3bd8a7d9c17c1f3
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2022-01-02 22:35:27 -0300

    simplifying inspect decorator

--- alura-courses/typescript-1-to-3/app/src/decorators/inspect.ts
+++ alura-courses/typescript-1-to-3/app/src/decorators/inspect.ts
@@ -1,20 +1,18 @@
-export function inspect() {
-	return function (
-		target: any,
-		propertyKey: string,
-		descriptor: PropertyDescriptor
-	) {
-		const originalMethod = descriptor.value
+export function inspect(
+	target: any,
+	propertyKey: string,
+	descriptor: PropertyDescriptor
+) {
+	const originalMethod = descriptor.value
 
-		descriptor.value = function (...args: any[]) {
-			console.log(`--- Method: ${propertyKey}`)
-			console.log(`--- Parameters: ${JSON.stringify(args)}`)
-			const originalReturn = originalMethod.apply(this, args)
-			console.log(`--- Returns: ${originalReturn}`)
+	descriptor.value = function (...args: any[]) {
+		console.log(`--- Method: ${propertyKey}`)
+		console.log(`--- Parameters: ${JSON.stringify(args)}`)
+		const originalReturn = originalMethod.apply(this, args)
+		console.log(`--- Returns: ${originalReturn}`)
 
-			return originalReturn
-		}
-
-		return descriptor
+		return originalReturn
 	}
+
+	return descriptor
 }
--- alura-courses/typescript-1-to-3/app/src/views/view.ts
+++ alura-courses/typescript-1-to-3/app/src/views/view.ts
@@ -17,7 +17,7 @@ export abstract class View<T> {
 		}
 	}
 
-	@inspect()
+	@inspect
 	@LogExecTime(true)
 	public update(model: T): void {
 		let template = this.template(model)
